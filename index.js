d5efe42026a6b8564115a7389bb03656a6fe90cc
ducviet2504 <your_email_address@example.com>
2021-03-22 00:58:20 +0700

    add navbar , creatr

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+
+var useRoute = require("./routes/user.route");
+var port = 3000;
+
+var app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "trang chu",
+  });
+});
+
+app.use("/users", useRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
